19a185f94d170869ad123f9d1ad28ff0cc3757c5
Muhammad Yusuf Habibie <myh481813@gmail.com>
2024-07-05 14:29:49 +0700

    Fix login API

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,9 +23,7 @@ class AuthController extends Controller
             return response()->json(['token' => $token], 200);
         }
 
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
-        ]);
+        return response()->json(['message' => 'Email atau Password yang anda masukkan salah'], 401);
     }
     public function logout(Request $request)
     {
